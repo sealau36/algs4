3db761760a78d15fcbafbd8a4f62cc4b6ef06280
Sean Lau <seanlau36@gmail.com>
2014-03-06 21:29:17 +0800

    init week 2 assignment

---
+++ 1.w2/Deque.java
@@ -0,0 +1,39 @@
+import java.util.Iterator;
+
+public class Deque<Item> implements Iterable<Item> {
+    // construct an empty deque
+    public Deque() {
+    }
+
+    // is the deque empty?
+    public boolean isEmpty() {
+    }
+
+    // return the number of items on the deque
+    public int size() {
+    }
+
+    // insert the item at the front
+    public void addFirst(Item item) {
+    }
+
+    // insert the item at the end
+    public void addLast(Item item) {
+    }
+
+    // delete and return the item at the front
+    public Item removeFirst() {
+    }
+
+    // delete and return the item at the end
+    public Item removeLast() {
+    }
+
+    // return an iterator over items in order from front to end
+    public Iterator<Item> iterator() {
+    }
+
+    // unit testing
+    public static void main(String[] args) {
+    }
+}
